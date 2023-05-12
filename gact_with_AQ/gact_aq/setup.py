31a0adf067bc3718b97e689f8879cc5e0cd21f6c
asdgasdf <133080986+asdgasdf@users.noreply.github.com>
2023-05-12 16:13:23 +0900

    gact_with_AQ

---
+++ gact_with_AQ/gact_aq/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from torch.utils import cpp_extension
+
+setup(name='gact_aq',
+      ext_modules=[
+          cpp_extension.CUDAExtension(
+              'gact_aq.cpp_extension.calc_precision',
+              ['gact_aq/cpp_extension/calc_precision.cc']
+          ),
+          cpp_extension.CUDAExtension(
+              'gact_aq.cpp_extension.minimax',
+              ['gact_aq/cpp_extension/minimax.cc', 'gact/cpp_extension/minimax_cuda_kernel.cu']
+          ),
+          cpp_extension.CUDAExtension(
+              'gact_aq.cpp_extension.quantization',
+              ['gact_aq/cpp_extension/quantization.cc',
+                  'gact_aq/cpp_extension/quantization_cuda_kernel.cu']
+          ),
+      ],
+      cmdclass={'build_ext': cpp_extension.BuildExtension},
+      packages=find_packages()
+      )
